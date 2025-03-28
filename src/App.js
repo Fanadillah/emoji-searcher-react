c35d94805303d4d18d3f93a5e491fbfda18cf620
Fanadillah <ilhampranataadi@gmail.com>
2025-03-28 14:34:28 +0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import axios from "axios";
 
 function App() {
   const [emojisData, setEmojisData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     async function fetchEmojis() {
-      const res = await axios.get('https://run.mocky.io/v3/27e16410-66b1-4507-8ae8-49618f237b0c')
+      setLoading(true)
+      try {
+        const res = await axios.get('https://run.mocky.io/v3/27e16410-66b1-4507-8ae8-49618f237b0c')
+        
+        setEmojisData(res.data)
+        setLoading(false)
 
-      console.log(res)
+      } catch (error) {
+        console.error(error)
+
+        setError(true)
+        setLoading(false)
+      }
     }
 
     fetchEmojis()
   }, [])
+
+  console.log('loading ?', loading)
+  console.log('error ?', error)
+  console.log('emojisData ?', emojisData)
 
   return (
     <div>

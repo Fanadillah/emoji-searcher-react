6f26e3bf4d0fdca76bcbb32ae08d5b36bea1de4a
Fanadillah <ilhampranataadi@gmail.com>
2025-03-28 14:20:55 +0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [emojisData, setEmojisData] = useState([])
+
+  useEffect(() => {
+    async function fetchEmojis() {
+      const res = await axios.get('https://run.mocky.io/v3/27e16410-66b1-4507-8ae8-49618f237b0c')
+
+      console.log(res)
+    }
+
+    fetchEmojis()
+  }, [])
+
   return (
     <div>
       <h1>Hello, world!</h1>

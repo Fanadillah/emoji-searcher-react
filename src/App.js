f78339afad3ef8f5badd10b9ac8327918a17f059
Fanadillah <ilhampranataadi@gmail.com>
2025-03-30 11:03:24 +0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     async function fetchEmojis() {
       setLoading(true)
       try {
-        const res = await axios.get('https://run.mocky.io/v3/27e16410-66b1-4507-8ae8-49618f237b0c')
+        const res = await axios.get('https://run.mocky.io/v3/09e96bef-4e5b-4246-86d3-cc7648c27923')
         
         setEmojisData(res.data)
         setLoading(false)
@@ -26,13 +26,15 @@ function App() {
     fetchEmojis()
   }, [])
 
-  console.log('loading ?', loading)
-  console.log('error ?', error)
-  console.log('emojisData ?', emojisData)
+
 
   return (
     <div>
       <h1>Hello, world!</h1>
+      {loading && <p>Loading....</p>}
+      {error && <p>Ooopsss....</p>}
+      {emojisData.length > 0 && <p>Emojis Data sukses!</p>}
+
     </div>
   );
 }
